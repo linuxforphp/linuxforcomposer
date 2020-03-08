c98e3a73deb35da86b7d2e400825c610d59213ed
Andrew Caya <andrewscaya@yahoo.ca>
2020-03-08 16:41:57 -0400

    2020-03-08 AC: Fixes an issue with volume paths on Windows.

--- src/Command/DockerManageCommand.php
+++ src/Command/DockerManageCommand.php
@@ -1492,11 +1492,15 @@ class DockerManageCommand extends Command
                     if (!empty($volumeMap)) {
                         if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
                             // @codeCoverageIgnoreStart
-                            if (strstr(php_uname('v'), 'Windows 10') === false && php_uname('r') != '10.0') {
+                            if (strstr(php_uname('v'), 'Windows 10') !== false && php_uname('r') == '10.0') {
+                                $volumeMap = $this->winNormalizePath($volumeMap);
+                            } else {
                                 $volumeMap = $this->winNormalizePath($volumeMap);
                                 $volumeMap = lcfirst($volumeMap);
-                                $volumeMap = str_replace(':/', '/', $volumeMap);
-                                $volumeMap = '/' . $volumeMap;
+                                if (strpos($volumeMap, ':') === 1) {
+                                    $volumeMap = substr_replace($volumeMap, '', 1, 1);
+                                    $volumeMap = '/' . $volumeMap;
+                                }
                             }
                             // @codeCoverageIgnoreEnd
                         }
