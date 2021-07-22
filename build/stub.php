94230d480a8e95c21cebd0cba813bb2ef6855834
Andrew Caya <andrewscaya@yahoo.ca>
2021-07-22 18:53:36 -0400

    2021-07-22 AC: Updates the version number.

--- build/stub.php
+++ build/stub.php
@@ -5,7 +5,7 @@
  * Linux for PHP/Linux for Composer
  *
  * Copyright 2017 - 2021 Foreach Code Factory <lfphp@asclinux.net>
- * Version 2.0.9.2
+ * Version 2.0.9.3
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
